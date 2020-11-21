b40af3ab8f0eb0f36e8f4ddeadba3410aee24202
Tanvee Balhara <balharatanvee@gmail.com>
2020-11-21 21:16:08 +0530

    initial commit

---
+++ helper.py
@@ -0,0 +1,60 @@
+from tables import *
+
+def decimalToBinary(n: str) -> str:
+    binary = bin(int(n)).replace("0b", "") 
+    if binary[0] == '-':
+        return '1' * (8 - len(binary) + 1) + ''.join(['1' if bit == '0' else '0' for bit in binary[1:]])
+    else:
+        return '0' * (8 - len(binary)) + binary
+
+def hexToBinary(n: str) -> str:  
+    binary = decimalToBinary(str(int(n, 16)))
+    return '0' * (8 - len(binary)) + binary
+
+def findAddressingMode(address: str) -> str :
+    if address[0] == '#':
+        return addressingModes['direct']
+    elif address[0] == '@':
+        return addressingModes['indirect']
+    elif address[0] == '$':
+        return addressingModes['relativePC']
+    elif address[0] == '+':
+        return addressingModes['base']
+    elif address[-1] == ')':
+        return addressingModes['registerIndirect']
+    elif address[-1] == '+':
+        return addressingModes['autoInc']
+    elif address in generalPurposeRegs:
+        return addressingModes['regsiter']
+    else:
+        return addressingModes['direct']
+
+def findAddressBits(address: str, addMode: str, addSymbol: dict) -> str:
+    if addMode == '1101':
+        return generalPurposeRegs[address]
+    elif addMode == '1110':
+        return generalPurposeRegs[address[1:-1]]
+    elif addMode == '1111':
+        return generalPurposeRegs[address[1:-2]]
+    elif address.isdecimal() or (addMode != '1001' and address[1:].isdecimal()):
+        return address
+    elif addMode == '1001':
+        return decimalToBinary(addSymbol[address])
+    else:
+        return decimalToBinary(addSymbol[address[1:]])
+
+def binaryForMRI(inst: list, LC: int, addSymbol: dict) -> str:
+    addMode = findAddressingMode(inst[-1])
+    opcode = MRI[inst[0]]
+    address = findAddressBits(inst[-1], addMode, addSymbol)
+    return [[decimalToBinary(int(LC)), addMode + opcode], [decimalToBinary(int(LC + 1)), address]]
+
+def binaryForNMRI_acc(inst: list, LC: int) -> str:
+    addMode = "0000"
+    opcode = NMRI_acc[inst[0]]
+    return [decimalToBinary(int(LC)), addMode + opcode]
+
+def binaryForNMRI_io(inst: list, LC: int) -> str:
+    addMode = "0111"
+    opcode = NMRI_io[inst[0]]
+    return [decimalToBinary(int(LC)), addMode + opcode]
---
+++ main.py
@@ -0,0 +1,58 @@
+from tables import *
+from helper import *
+
+
+# FIRST PASS
+
+LC = 0
+addSymbol = dict()
+
+with open("machineCode.txt", mode='r') as f:
+    for line in f:
+        inst = line.split()
+        if line[0] == ".":
+            addSymbol.update({inst[0][1:]:LC})
+        else:
+            oprn = inst[0]
+            if oprn == 'ORG':
+                LC = int(inst[1])
+                continue
+            elif oprn == 'END':
+                break
+            elif oprn in MRI:
+                LC += 1
+        LC += 1
+
+
+# SECOND PASS
+
+LC = 0
+binary_code = []
+
+with open("machineCode.txt", mode='r') as f:
+    for line in f:
+        inst = line.split('/')[0].split()
+        if line[0] == ".":
+            if inst[1] == 'DEC':
+                binary_code.append([decimalToBinary(addSymbol[inst[0][1:]]), decimalToBinary(inst[-1])])
+            elif inst[1] == 'HEX':
+                binary_code.append([decimalToBinary(addSymbol[inst[0][1:]]), hexToBinary(inst[-1])])
+        else:
+            oprn = inst[0]
+            if oprn == 'ORG':
+                LC = int(inst[1])
+                continue
+            elif oprn == 'END':
+                break
+            elif oprn in MRI:
+                binary_code += binaryForMRI(inst, LC, addSymbol)
+                LC += 1
+            elif oprn in NMRI_io:
+                binary_code.append(binaryForNMRI_io(inst, LC))
+            elif oprn in NMRI_acc:
+                binary_code.append(binaryForNMRI_acc(inst, LC))
+        LC += 1
+
+with open("binarycode.txt", mode='w') as f:
+    f.write('LOCATION\t\tCONTENT\n\n')
+    f.writelines([ins[0] + '\t\t' + ins[1] + '\n' for ins in binary_code])
