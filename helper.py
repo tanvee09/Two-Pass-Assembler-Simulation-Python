dc8f5d139b7341603aa31c96ccf56b39e32e0069
Tanvee Balhara <balharatanvee@gmail.com>
2020-11-28 16:22:54 +0530

    added error handling

--- helper.py
+++ helper.py
@@ -1,4 +1,11 @@
 from tables import *
+import re
+
+
+def callError(lineNo: int, errMessage: str) :
+    print('\033[91m' + 'Error at line {}: {}'.format(lineNo, errMessage) + '\033[0m')
+    exit(1)
+
 
 def toBinary(n, b: int) -> str:
     binary = bin(int(str(n), b)).replace("0b", "")
@@ -6,6 +13,7 @@ def toBinary(n, b: int) -> str:
         return '1' * (8 - len(binary) + 1) + ''.join(['1' if bit == '0' else '0' for bit in binary[1:]])
     else:
         return '0' * (8 - len(binary)) + binary
+
 
 def findAddressingMode(address: str) -> str :
     if address[0] == '#':
@@ -25,32 +33,51 @@ def findAddressingMode(address: str) -> str :
     else:
         return addressingModes['direct']
 
-def findAddressBits(address: str, addMode: str, addSymbol: dict) -> str:
-    if addMode == '1101':
+
+def findAddressBits(address: str, addMode: str, addSymbol: dict, lineNo: int) -> str:
+    if addMode == '1101': # Register
+        if address not in generalPurposeRegs.keys():
+            callError(lineNo, "Referenced Register does not exist")
         return generalPurposeRegs[address]
-    elif addMode == '1110':
+
+    elif addMode == '1110': # registerIndirect
+        if address not in generalPurposeRegs.keys():
+            callError(lineNo, "Referenced Register does not exist")
         return generalPurposeRegs[address[1:-1]]
-    elif addMode == '1111':
+
+    elif addMode == '1111': # autoInc
+        if address not in generalPurposeRegs.keys():
+            callError(lineNo, "Referenced Register does not exist")
         return generalPurposeRegs[address[1:-2]]
-    elif addMode == '1001' and address.isdecimal():
-        return address
-    elif addMode != '1001' and address[1:].isdecimal():
+
+    elif addMode == '1000': # immediate
+        if len(address) != 8 or re.fullmatch("[01]*", address) == None:
+            callError(lineNo, "Address should be in binary")
         return toBinary(address[1:], 2)
-    elif addMode == '1001':
+
+    if addMode != '1001':# not direct
+        address = address[1:]
+
+    if len(address) == 8 and re.fullmatch("[01]*", address) != None:
+        return toBinary(address, 2)
+    elif address in addSymbol.keys():
         return toBinary(addSymbol[address], 10)
     else:
-        return toBinary(addSymbol[address[1:]], 10)
+        callError(lineNo, "Address should be in binary")
 
-def binaryForMRI(inst: list, LC: int, addSymbol: dict) -> str:
+
+def binaryForMRI(inst: list, LC: int, addSymbol: dict, lineNo: int) -> str:
     addMode = findAddressingMode(inst[-1])
     opcode = MRI[inst[0]]
-    address = findAddressBits(inst[-1], addMode, addSymbol)
+    address = findAddressBits(inst[-1], addMode, addSymbol, lineNo)
     return [[toBinary(int(LC), 10), addMode + opcode], [toBinary(int(LC + 1), 10), address]]
+
 
 def binaryForNMRI_acc(inst: list, LC: int) -> str:
     addMode = "0000"
     opcode = NMRI_acc[inst[0]]
     return [toBinary(int(LC), 10), addMode + opcode]
+
 
 def binaryForNMRI_io(inst: list, LC: int) -> str:
     addMode = "0111"
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import re
 from tables import *
 from helper import *
+
+
 
 
 # FIRST PASS
@@ -7,12 +10,32 @@ from helper import *
 LC = 0
 addSymbol = dict()
 
+lineNo = 0
+
 with open("machineCode.txt", mode='r') as f:
     for line in f:
-        inst = line.split()
+        lineNo += 1
+        inst = line.split('/')[0].split()
         if line[0] == ".":
-            addSymbol.update({inst[0][1:]:LC})
+            label = inst[0][1:]
+            # label repeated
+            if label in addSymbol:
+                callError(lineNo, "Labels cannot be repeated ")
+            # label length > 3
+            elif len(label) > 3:
+                callError(lineNo, "Labels cannot exceed length 3")
+            # Same as register names (R1, R2, R3, R4)
+            x = re.fullmatch("R1|R2|R3|R4", label)
+            if x:
+                callError(lineNo, "Label can not be Register name")
+            # Should be alphanumeric and should start with an alphabet
+            x = re.fullmatch("[a-zA-Z][a-zA-Z0-9]*", label)
+            if x == None:
+                callError(lineNo, "Label should be alphanumeric and should start with an alphabet")
+            addSymbol.update({label:LC})
         else:
+            if len(inst) == 0 :
+                callError(lineNo,"Line can not be empty or can not start with a comment, it must start with an instruction or a label")
             oprn = inst[0]
             if oprn == 'ORG':
                 LC = int(inst[1])
@@ -30,19 +53,42 @@ with open("machineCode.txt", mode='r') as f:
 LC = 0
 binary_code = []
 
+lineNo = 0
+
 with open("machineCode.txt", mode='r') as f:
     for line in f:
+        lineNo += 1
         inst = line.split('/')[0].split()
-        
+
+        if len(inst) == 0 :
+            callError(lineNo,"Line can not be empty or can not start with a comment, it must start with an instruction or a label")
+
+        #
+
         if line[0] == ".":
             if inst[1] == 'DEC':
+                try:
+                    int(inst[-1])
+                except:
+                    callError(lineNo, "Value at label should be decimal")
+                # if not inst[-1].isdecimal():
+                #     callError(lineNo, "Value at label cannot contain characters")
+                if int(inst[-1]) > (2**7 - 1) or int(inst[-1]) < -2**7 + 1:
+                    callError(lineNo, "Value out of bounds")
                 binary_code.append([toBinary(addSymbol[inst[0][1:]], 10), toBinary(inst[-1], 10)])
                 continue
             elif inst[1] == 'HEX':
+                try:
+                    int(inst[-1], 16)
+                except:
+                    callError(lineNo, "Value at label should be hexadecimal")
+                if int(inst[-1], 16) > (2**7 - 1) or int(inst[-1], 16) < -2**7 + 1: # -7F FF
+                    callError(lineNo, "Value out of bounds")
                 binary_code.append([toBinary(addSymbol[inst[0][1:]], 10), toBinary(inst[-1], 16)])
                 continue
             else:
                 inst = inst[1:]
+
 
         oprn = inst[0]
         if oprn == 'ORG':
@@ -51,14 +97,25 @@ with open("machineCode.txt", mode='r') as f:
         elif oprn == 'END':
             break
         elif oprn in MRI:
-            binary_code += binaryForMRI(inst, LC, addSymbol)
+            binary_code += binaryForMRI(inst, LC, addSymbol, lineNo)
             LC += 1
         elif oprn in NMRI_io:
+            if len(inst) > 1:
+                callError(lineNo, "NMRI instructions only contain opcode")
             binary_code.append(binaryForNMRI_io(inst, LC))
         elif oprn in NMRI_acc:
+            if len(inst) > 1:
+                callError(lineNo, "NMRI instructions only contain opcode")
             binary_code.append(binaryForNMRI_acc(inst, LC))
+        else:
+            callError(lineNo, "Opcode not found")
         LC += 1
+        if LC >= 2**8:
+            callError(lineNo, "Program has run out of memor")
+
 
 with open("binarycode.txt", mode='w') as f:
     f.write('LOCATION\t\tCONTENT\n\n')
     f.writelines([ins[0] + '\t\t' + ins[1] + '\n' for ins in binary_code])
+
+print('\033[92m' + 'Program has been successfully assembled' + '\033[0m')
